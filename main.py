b17496dfa8110ab093bb934086733f0e5efe4d89
quangcog <145032282+quangcog@users.noreply.github.com>
2023-09-14 14:25:40 +0000

    add message
    say hello to me

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ while True:
   current_time = time.localtime()
   formatted_time = time.strftime("%H:%M:%S", current_time)
   print(formatted_time, end="\r")
+  print("Hello quangcog")
   time.sleep(1)
